2d581009d113b44117146ce605cc67c5b230be75
waqar-313 <157794826+waqar-313@users.noreply.github.com>
2026-05-13 13:23:12 +0500

    replace bcrypt with bcryptjs

--- backend/Controllers/adminController.js
+++ backend/Controllers/adminController.js
@@ -1,5 +1,5 @@
 const Admin = require('../Models/Admin/AdminAuth');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require("../Models/Users"); // or Supervisor/Admin model
 const Project =  require("../Models/SupervisorModels/Project");
